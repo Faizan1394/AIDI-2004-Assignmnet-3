245a0e853b5abb89e0e3b1ef94b368221075143a
Faizan Ali <m.faizan.ali13@gmail.com>
2021-03-28 00:12:46 -0400

    Combined Hibba/Faizan Sections

--- restfulAPI.py
+++ restfulAPI.py
@@ -5,6 +5,12 @@ import sqlite3
 app = Flask(__name__)
 api = Api(app)
 
+
+#arguments required for get reqest to get a specific students info
+getDataArgs = reqparse.RequestParser()
+getDataArgs.add_argument("studentID",type=int,help="Student Number",required=True)
+
+
 #arguments required for put request to add data
 addDataArgs = reqparse.RequestParser()
 addDataArgs.add_argument("studentID",type=int,help="Student Number",required=True)
@@ -13,15 +19,58 @@ addDataArgs.add_argument("lastName",type=str,help="Student last name",required=T
 addDataArgs.add_argument("dob",type=str,help="Student Date of Birth",required=True)
 addDataArgs.add_argument("amountDue",type=str,help="Amount Student owes",required=True)
 
+
+#arguments required for delete request to delete data
+delDataArgs = reqparse.RequestParser()
+delDataArgs.add_argument("studentID",type=int,help="Student Number",required=True)
+
+#arguments required for put request to update data
+putDataArgs = reqparse.RequestParser()
+putDataArgs.add_argument("studentID",type=int,help="Student Number",required=True)
+putDataArgs.add_argument("firstName",type=str,help="Student first name",required=True)
+putDataArgs.add_argument("lastName",type=str,help="Student last name",required=True)
+putDataArgs.add_argument("dob",type=str,help="Student Date of Birth",required=True)
+putDataArgs.add_argument("amountDue",type=str,help="Amount Student owes",required=True)
+
+
 #for showing all records
 class dbmAll(Resource):
     def get(self):
-        pass
+        #create connection to database. Will be created if it doesnt exist
+        conn = sqlite3.connect('studentInfo.db')
+
+        #create cursor instance
+        c = conn.cursor()
+
+        c.execute("SELECT studentID, firstName, lastName, dateOfBirth, amountDue FROM Student")
+        a = c.fetchall()
+
+        #save the changes
+        conn.commit()
+        #close the connection
+        conn.close()
+
+        return  {"All records":a}
 
 #for add/update/delete/and view database records
 class dbm(Resource):
     def get(self):
-        return  {"name":"hello world"}
+        args = getDataArgs.parse_args()
+        #create connection to database. Will be created if it doesnt exist
+        conn = sqlite3.connect('studentInfo.db')
+
+        #create cursor instance
+        c = conn.cursor()
+
+        c.execute("SELECT studentID, firstName, lastName, dateOfBirth, amountDue FROM Student")
+        a = c.fetchone()
+
+        #save the changes
+        conn.commit()
+        #close the connection
+        conn.close()
+
+        return  {args}
 
     def post(self):
         args = addDataArgs.parse_args()
@@ -43,10 +92,34 @@ class dbm(Resource):
         return  {"Added":args}
 
     def put(self):
-        return  {"name":"hello world 3"}
+        args = putDataArgs.parse_args()
+
+        conn = sqlite3.connect('studentInfo.db')
+        c = conn.cursor()
+
+        c.execute("""UPDATE Student SET firstName=?, lastName=?, dateOfBirth=?, amountDue=? WHERE studentID=? """,
+                   (args["firstName"],args["lastName"],args["dob"],args["amountDue"],args["studentID"]))
+
+
+        conn.commit()
+        conn.close()
+
+        return  {"UPDATED":args}
+
 
     def delete(self):
-        return  {"name":"hello world 4"}
+
+        args = delDataArgs.parse_args()
+
+        conn = sqlite3.connect('studentInfo.db')
+        c = conn.cursor()
+
+        c.execute("DELETE FROM Student WHERE studentID=?", (args["studentID"],))
+
+        conn.commit()
+        conn.close()
+
+        return  {"Deleted":args}
 
 
 
